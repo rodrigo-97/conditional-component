14050c953ca0302a779a68ae02c3bafbc1a8b52b
rodrigo araujo <rodmatheus99@gmail.com>
2022-04-07 00:35:55 -0300

    add proptypes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <Conditional 
-        condition={1 === 1}
+        condition={true}
         ifTrueComponent={<p>Rodrigo</p>}
         elseComponent={<p>araujo</p>}
       />
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,15 @@
+import React from "react";
+import PropTypes from 'prop-types'
 export const Conditional = (props) => {
   console.log(props);
   const { condition, ifTrueComponent, elseComponent } = props;
 
   return <>{condition ? <>{ifTrueComponent}</> : <>{elseComponent}</>}</>;
 };
+
+
+Conditional.propTypes = {
+  condition: PropTypes.any.isRequired,
+  ifTrueComponent: PropTypes.element.isRequired,
+  elseComponent: PropTypes.element,
+}
